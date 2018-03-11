ecd080a060cfebab40d7ae925b2c7b6c90a1c8a0
Troy Alford <troy@troyalford.com>
2018-03-11 13:08:23 -0700

    Update story options & title

--- stories/Tengwar.js
+++ stories/Tengwar.js
@@ -1,23 +1,52 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, select, text } from '@storybook/addon-knobs/react'
-import Tengwar from '../source/components/Tengwar'
+import Tengwar, { MODES } from '../source/components/Tengwar'
 
 const stories = storiesOf('Tengwar', module)
 
 stories.addDecorator(withKnobs)
 
-const LANGUAGES = {
-  'adunaic': 'Adûnaic',
-  'blackspeech': 'Black Speech',
-  'quenya': 'Quenya',
-  'sindarin': 'Sindarin',
-  'telerin': 'Telerin',
+const LANGUAGES = MODES.reduce((map, mode) => ({
+  ...map,
+  [mode.name]: mode.humanName
+}), {})
+const TYPEFACE_OPTIONS = {
+  annatar: 'Tengwar Annatar',
+  eldamar: 'Tengwar Eldamar',
+  elfica: 'Tengwar Elfica',
+  parmaite: 'Tengwar Parmaite',
+  sindarin: 'Tengwar Sindarin',
+}
+const WRITING_OPTIONS = {
+  cirth: 'Cirth',
+  gothic: 'Gothic',
+  runes: 'Runes',
+  sarati: 'Sarati',
+  tengwar: 'Tengwar',
 }
 
-stories.add('quenya basic example', () => (
+const DEFAULT_TEXT = `
+Ai ! laurië lantar lassi súrinen ,
+yéni únótimë ve rámar aldaron !
+Yéni ve lintë yuldar avánier
+mi oromardi lissë-miruvóreva
+Andúnë pella , Vardo tellumar
+nu luini yassen tintilar i eleni
+ómaryo airetári-lírinen .
+
+Sí man i yulma nin enquantuva ?
+`
+
+stories.add('Full Example', () => (
   <Tengwar
+    children={text('Text', DEFAULT_TEXT, 'foo')}
     language={select('Language', LANGUAGES, 'Quenya', 'foo')}
-    children={text('Text', 'An Elven Phrase', 'foo')}
+    typeface={select('Typeface', TYPEFACE_OPTIONS, 'Tengwar Elfica', 'foo')}
+    writing={select('Writing', WRITING_OPTIONS, 'Tengwar', 'foo')}
+    options={{
+      always_use_romen_for_r: true,
+      split_diphthongs: true,
+    }}
   />
 ))
