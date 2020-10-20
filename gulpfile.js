16a3b546a65b1c8777d00840e0f727d2a0105226
Troy Alford <talford@vestmark.com>
2020-10-19 23:37:19 -0700

    Refactor font inclusions

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 
 const PATH = {
-  JS: './node_modules/glaemscribe/build/web/glaemscribe/js',
+	FONTS: './node_modules/glaemscribe-fonts/fonts/build/webs',
+	JS: './node_modules/glaemscribe-fonts/build/web/glaemscribe/js',
 }
 
-gulp.task('default', () => (
-  gulp.src([
-    `${PATH.JS}/glaemscribe.js`,
-    `${PATH.JS}/charsets/*.cst.js`,
+gulp.task('shim-js', () => (
+	gulp.src([
+		'./vendor/glaemscribe.shim.start.js',
+		`${PATH.JS}/glaemscribe.js`,
+		`${PATH.JS}/charsets/*.cst.js`,
 		`${PATH.JS}/modes/*.glaem.js`,
-    `./vendor/glaemscribe.shim.js`,
-  ]).pipe(concat('glaemscribe.built.js'))
-    .pipe(gulp.dest('./dist'))
+		'./vendor/glaemscribe.shim.end.js',
+	]).pipe(concat('glaemscribe.built.js'))
+		.pipe(gulp.dest('./temp'))
 ))
+
+gulp.task('copy-font-css', () => (
+	gulp.src([
+		`${PATH.FONTS}/*.css`,
+		'./vendor/glaemscribe.classes.scss',
+	]).pipe(concat('glaemscribe.built.css'))
+		.pipe(gulp.dest('./temp'))
+))
+gulp.task('copy-fonts', () => (
+	gulp.src([
+		`${PATH.FONTS}/*.{eot,svg,ttf,woff}`,
+	]).pipe(gulp.dest('./temp'))
+))
+
+gulp.task('default', gulp.parallel('shim-js', 'copy-fonts', 'copy-font-css'))
