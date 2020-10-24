4cfc36af564aad7f3fb26ad58f3507bc5f434d8f
Troy Alford <talford@vestmark.com>
2020-10-23 21:47:33 -0700

    Move docs

--- source/docs/index.tsx
+++ source/docs/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Tengwar } from '../source/components/Tengwar'
+import { Tengwar } from '~/components/Tengwar'
 
 ReactDOM.render(
 	<Tengwar>
